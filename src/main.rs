df465e8822f82903dc6625e336cdc67ac9482a20
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-10-28 13:15:37 +0100

    Add prefetching variants

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use sux::{func::VBuilder, utils::FromIntoIterator};
 use clap::Parser;
 use ph::phast;
 use ptr_hash::{PtrHash, PtrHashParams};
-use rand::{RngCore, SeedableRng, rngs::SmallRng, seq::SliceRandom};
+use rand::{rngs::SmallRng, seq::SliceRandom, RngCore, SeedableRng};
 
 #[derive(Parser, Debug)]
 #[command(about = "Build contributor and origin collaboration graphs")]
@@ -36,7 +36,7 @@ fn main() {
         let _: _ = black_box(*unsafe { map.get(&keys[i]).unwrap_unchecked() });
     }
     println!(
-        "GxHM     lookup time {:8.3}ns",
+        "GxHM             lookup time {:8.3}ns",
         now.elapsed().as_nanos() as f64 / n as f64
     );
 
@@ -57,11 +57,37 @@ fn main() {
         _ = black_box(values[ptr_hash.index(&keys[i])]);
     }
     println!(
-        "PtrHash  lookup time {:8.3}ns",
+        "PtrHash          lookup time {:8.3}ns",
+        now.elapsed().as_nanos() as f64 / n as f64
+    );
+
+    let now = Instant::now();
+    index_all_with_prefetch(&values, keys.iter().map(|key| ptr_hash.index(key)));
+    println!(
+        "PtrHash +pf vals lookup time {:8.3}ns",
+        now.elapsed().as_nanos() as f64 / n as f64
+    );
+
+    let now = Instant::now();
+    let indices = ptr_hash.index_stream::<32, true, _>(&keys);
+    indices.for_each(|idx| {
+        _ = black_box(values[idx]);
+    });
+    println!(
+        "PtrHash +pf idx  lookup time {:8.3}ns",
+        now.elapsed().as_nanos() as f64 / n as f64
+    );
+
+    let now = Instant::now();
+    index_all_with_prefetch(&values, ptr_hash.index_stream::<32, true, _>(&keys));
+    println!(
+        "PtrHash +pf both lookup time {:8.3}ns",
         now.elapsed().as_nanos() as f64 / n as f64
     );
 
     drop(ptr_hash);
+
+    // Phast
 
     let phast = phast::Function2::from_slice_mt(&keys);
 
@@ -79,7 +105,14 @@ fn main() {
         _ = black_box(values[phast.get(&keys[i])]);
     }
     println!(
-        "Phast    lookup time {:8.3}ns",
+        "Phast            lookup time {:8.3}ns",
+        now.elapsed().as_nanos() as f64 / n as f64
+    );
+
+    let now = Instant::now();
+    index_all_with_prefetch(&values, keys.iter().map(|key| phast.get(key)));
+    println!(
+        "Phast +pf vals   lookup time {:8.3}ns",
         now.elapsed().as_nanos() as f64 / n as f64
     );
 
@@ -99,7 +132,41 @@ fn main() {
         _ = black_box(vfunc.get(&keys[i]));
     }
     println!(
-        "VFunc    lookup time {:8.3}ns",
+        "VFunc            lookup time {:8.3}ns",
         now.elapsed().as_nanos() as f64 / n as f64
     );
+}
+
+#[inline(always)]
+fn index_all_with_prefetch(values: &[u64], indices: impl IntoIterator<Item = usize>) {
+    let mut buf = [0usize; 32];
+    let mut i = 0;
+    indices.into_iter().for_each(|idx| {
+        _ = black_box(values[buf[i % 32]]);
+        buf[i % 32] = idx;
+        prefetch_index(&values, idx);
+        i += 1;
+    });
+}
+
+/// Prefetch the given cacheline into L1 cache.
+pub(crate) fn prefetch_index<T>(s: &[T], index: usize) {
+    let ptr = s.as_ptr().wrapping_add(index) as *const u64;
+    #[cfg(target_arch = "x86_64")]
+    unsafe {
+        std::arch::x86_64::_mm_prefetch(ptr as *const i8, std::arch::x86_64::_MM_HINT_T0);
+    }
+    #[cfg(target_arch = "x86")]
+    unsafe {
+        std::arch::x86::_mm_prefetch(ptr as *const i8, std::arch::x86::_MM_HINT_T0);
+    }
+    #[cfg(target_arch = "aarch64")]
+    unsafe {
+        // TODO: Put this behind a feature flag.
+        // std::arch::aarch64::_prefetch(ptr as *const i8, std::arch::aarch64::_PREFETCH_LOCALITY3);
+    }
+    #[cfg(not(any(target_arch = "x86_64", target_arch = "x86", target_arch = "aarch64")))]
+    {
+        // Do nothing.
+    }
 }
